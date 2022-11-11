6fa78cc87a423ddc7212e8e28c389b18cbf412d9
furkanevin <furkanevin00@gmail.com>
2022-11-11 19:19:43 +0300

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import '.././style.scss';
 const Header = () => {
   return (
-    <nav className="navbar  navbar-expand navbar-dark bg-dark">
-      <div className="nav navbar-nav">
-        <Link to="/" className="nav-item nav-link active" aria-current="page">
-          Home
-        </Link>
+    <div className="header">
+      <div className="browse">
+        <div className="browse-category">
+          Browse Category
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="feather feather-chevron-down"
+          >
+            <path d="M6 9l6 6 6-6" />
+          </svg>
+        </div>
+        <div className="search-bar">
+          <input type="text" placeholder="Search Book" />
+        </div>
       </div>
-    </nav>
+      <Link to="/" className="header-title">
+        read<span>books</span>
+      </Link>
+      <div className="profile">
+        <div className="user-profile">
+          <img
+            src="https://randomuser.me/api/portraits/women/63.jpg"
+            alt=""
+            className="user-img"
+          />
+        </div>
+        <div className="profile-menu">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="feather feather-menu"
+          >
+            <path d="M3 12h18M3 6h18M3 18h18" />
+          </svg>
+          Menu
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
+import BookDetails from '../components/BookDetails';
 import Header from '../components/Header';
 import ListBooks from './../components/ListBooks';
 
 const Home = () => {
   return (
-    <div>
+    <div className="home">
       <Header />
       <h1>Home Page</h1>
       <ListBooks />
